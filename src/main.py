d2bbe23af3299a390f78b65d26d132b6f3f67855
Luke Snyder <ls008@uark.edu>
2018-01-30 18:49:37 -0600

    Added file and help menus.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction
 from PyQt5.QtGui import QIcon
- 
-class App(QWidget):
- 
+
+class App(QMainWindow):
+
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.title = 'VFI Analysis Application'
+        self.left = 350
+        self.top = 150
+        self.width = 800
+        self.height = 600
         self.initUI()
- 
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        mainMenu = self.menuBar()
+        mainMenu.setNativeMenuBar(False) #For MacOS
+        fileMenu = mainMenu.addMenu('File')
+        helpMenu = mainMenu.addMenu('Help')
+
+        uploadVideoAction = QAction('Upload Video', self)
+        uploadVideoAction.setStatusTip('Upload Video')
+
+        uploadPhotoAction = QAction('Upload Photo', self)
+        uploadPhotoAction.setStatusTip('Upload Photo')
+
+        exitAction = QAction('Exit', self)
+        exitAction.setStatusTip('Exit Program')
+        exitAction.triggered.connect(self.close)
+
+        fileMenu.addAction(uploadVideoAction)
+        fileMenu.addAction(uploadPhotoAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(exitAction)
+
         self.show()
- 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
