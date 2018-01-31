4f3edbd44da50295e8f3f0fde9b362931b147bc9
Luke Snyder <ls008@uark.edu>
2018-01-30 20:16:39 -0600

    Add video playing.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction
+from PyQt5.QtWidgets import (QMainWindow, QPushButton, QWidget, QAction,
+    QApplication, QFileDialog, QHBoxLayout, QLabel, QSizePolicy, QSlider,
+    QStyle, QVBoxLayout)
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtGui import QIcon
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'VFI Analysis Application'
+        self.title = 'VFI Analysis'
         self.left = 350
         self.top = 150
         self.width = 800
@@ -16,6 +21,21 @@ class App(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videoWidget = QVideoWidget()
+
+        self.playButton = QPushButton()
+        self.playButton .setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+
+        self.errorLabel = QLabel()
+        self.errorLabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
         mainMenu = self.menuBar()
         mainMenu.setNativeMenuBar(False) #For MacOS
@@ -24,9 +44,13 @@ class App(QMainWindow):
 
         uploadVideoAction = QAction('Upload Video', self)
         uploadVideoAction.setStatusTip('Upload Video')
+        uploadVideoAction.triggered.connect(self.openVideoFile)
 
         uploadPhotoAction = QAction('Upload Photo', self)
         uploadPhotoAction.setStatusTip('Upload Photo')
+
+        analyzeAreaAction = QAction('Analyze Area', self)
+        analyzeAreaAction.setStatusTip('Analyze Area')
 
         exitAction = QAction('Exit', self)
         exitAction.setStatusTip('Exit Program')
@@ -35,9 +59,66 @@ class App(QMainWindow):
         fileMenu.addAction(uploadVideoAction)
         fileMenu.addAction(uploadPhotoAction)
         fileMenu.addSeparator()
+        fileMenu.addAction(analyzeAreaAction)
+        fileMenu.addSeparator()
         fileMenu.addAction(exitAction)
 
+        widget = QWidget(self)
+        self.setCentralWidget(widget)
+
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(0, 0, 0, 0)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.positionSlider)
+
+        layout = QVBoxLayout()
+        layout.addWidget(videoWidget)
+        layout.addLayout(controlLayout)
+        layout.addWidget(self.errorLabel)
+
+        widget.setLayout(layout)
+
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.stateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+
         self.show()
+
+    def openVideoFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Video", QDir.homePath())
+
+        if fileName != '':
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
+            self.errorLabel.setText('')
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def stateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.errorLabel.setText('ERROR: ' + self.mediaPlayer.errorString() + '.')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
