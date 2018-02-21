4ef794563f8da2854c0809242c81e1199701076d
Luke Snyder <ls008@uark.edu>
2018-02-21 11:38:49 -0600

    Cropped images.

--- frameOutput/script.py
+++ frameOutput/script.py
@@ -4,6 +4,7 @@ count = 0
 
 while count < cap.get(cv2.CAP_PROP_FRAME_COUNT):
     ret,frame = cap.read()
+    frame = frame[0:0+725, 188:188+465]
     cv2.imwrite("C:/Users/snyde/Documents/Capstone/frameOutput/frames/frame%d.jpg" % count, frame)
     count = count + 1
     if cv2.waitKey(10) & 0xFF == ord('q'):
